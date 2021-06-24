8b83a537f88e8f05e6c354acf22dc85512ef3d8d
Edita Pesa <editapesa@gmail.com>
2021-06-23 21:57:31 -0400

    add code for Col.js and start a css file for it, add import css file

--- empdirectory/src/components/Col.js
+++ empdirectory/src/components/Col.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import '../styles/Col.css';
 
 function Col(props) {
-    const size = props.size.split('').map(size => 'col-' + size).join('');
-
-    return <div className={size}>{props.children}</div>;
+    return (
+        <div className='row'>
+            <div className='img-col'>
+                <strong>Image</strong>
+                <img alt={props.name} src={props.image} />
+            </div>
+            <div className='name-col'>
+                <strong>Name</strong> {props.name}
+            </div>
+            <div className='phone-col'>
+                <strong>Phone</strong> {props.phone}
+            </div>
+            <div className='email-col'>
+                <strong>Email</strong> {props.email}
+            </div>
+            <div className='dob-col'>
+                <strong>DOB</strong> {props.dob}
+            </div>
+        </div>
+    );
 }
 
 export default Col;
