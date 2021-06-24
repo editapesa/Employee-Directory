dbe7b84eefe9c8b078705d9a64929d8ef650ec54
Edita Pesa <editapesa@gmail.com>
2021-06-23 20:19:46 -0400

    add Component class to Header.js

--- empdirectory/src/components/Header.js
+++ empdirectory/src/components/Header.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Header() {
-    return (
-        <div className='jumbotron text-center'>
-            <h1>Employee Directory</h1>
-            <p>Click on an arrow to filter by heading or use the search box to narrow your results.</p>
-        </div>
-    );
+class Header extends Component {
+    render() {
+        return (
+            <div className='header'>
+                <h1>Employee Directory</h1>
+                <p>Click on an arrow to filter by heading or use the search box to narrow your results.</p>
+            </div>
+        )
+    }
 }
 
 export default Header;
